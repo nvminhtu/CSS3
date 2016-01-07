b2d444aa6c25c6aba3a16fe2e0573c37a6cef4b0
Tu Nguyen <nvminhtu@gmail.com>
2016-01-07 23:07:41 +0700

    add node sass and 2 new demos for CSS3

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,25 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
 
 // Static Server + watching scss/html files
-gulp.task('serve', function() {
+gulp.task('serve', ['sass'], function() {
 
     browserSync.init({
         server: "./"
     });
 
-    gulp.watch("*.html").on('change', browserSync.reload);
-    gulp.watch("*.css", ['sass']);
+    gulp.watch("demo/scss/*.scss", ['sass']);
+    //gulp.watch("/*.html").on('change', browserSync.reload);
+    gulp.watch("*/**/*.html").on('change', browserSync.reload);
 });
 
+// Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("*.css")
-        .pipe(reload({stream: true}));
+    return gulp.src("demo/scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("demo/css"))
+        .pipe(browserSync.stream());
 });
-
 
 gulp.task('default', ['serve']);
